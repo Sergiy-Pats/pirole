45d7414789d243d08218058f64d664475ee6480a
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-15 23:10:35 +0200

    1

--- js/script.js
+++ js/script.js
@@ -3,5 +3,18 @@ jQuery('document').ready(function () {
 		jQuery('.header__burger,.header__menu,.header__block').toggleClass('active');
 
 	})
+	jQuery('.gallery__item').click(function (event) {
+		jQuery('body').toggleClass('lock');
+	})
+	jQuery('.close').click(function (close) {
+		jQuery('body').removeClass('lock');
+	})
+	jQuery('.poppup').click(function (event) {
+		window.location = jQuery('.close').attr('href');
+	});
 })
 
+
+
+
+//jQuery('body').toggleClass('lock');
