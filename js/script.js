2872718d2ad94cc50081657c6b48003f59b1f688
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-15 22:43:16 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+jQuery('document').ready(function () {
+	jQuery('.header__burger').click(function (event) {
+		jQuery('.header__burger,.header__menu,.header__block').toggleClass('active');
+
+	})
+})
+
